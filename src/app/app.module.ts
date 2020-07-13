1d9c9529f6693d5fa9207ad1c496ae0936a8da18
Nick Robinson <nickr964@gmail.com>
2020-07-13 17:14:02 -0400

    pushing app so far

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatSliderModule } from '@angular/material/slider';
+import {  MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReviewComponent } from './review/review.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReviewComponent
   ],
   imports: [
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule, // for database
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebase: {
+      apiKey: 'AIzaSyCycBtI9T84sLISJ_ScG4pr1Gg9NFguPWE',
+      authDomain: 'tuff-puffin-movie-buffs.firebaseapp.com',
+      databaseURL: 'https://tuff-puffin-movie-buffs.firebaseio.com',
+      projectId: 'tuff-puffin-movie-buffs',
+      storageBucket: 'tuff-puffin-movie-buffs.appspot.com',
+      messagingSenderId: '489747071356',
+      appId: '1:489747071356:web:bd9c50b568a81c755324f9',
+      measurementId: 'G-X1KHX3L107'
+  }
 };
 
 /*
